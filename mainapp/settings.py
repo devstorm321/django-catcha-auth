6b52dbcb3dfa19cc7668132718affa00091ba416
devstorm321 <ddstorm0114@outlook.com>
2022-01-18 21:32:16 +0300

    requirements.txt update

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -153,3 +153,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
